a1d7e8fee3016518673d912173c6bbc0997eed57
Ihor Shchepilov <i.shchepilov@sysiq.com>
2015-02-17 18:01:10 +0200

    fix first press key

--- js/app.js
+++ js/app.js
@@ -1,14 +1,22 @@
+var maxSymbol = 140;
 var leftTotal = document.getElementById('js-message-left-symbols');
 var messageArea = document.getElementById('js-message');
 var total = document.getElementById('js-message-total');
-var maxSymbol = 140;
+
+leftTotal.innerHTML = maxSymbol;
 
 function symbolCounter(event) {
-    var messageLength = messageArea.value.length;
-    var leftTotalLength = maxSymbol - messageLength;
+    var messageLength = messageArea.value.length + 1;
+    var leftTotalLength = maxSymbol - messageLength - 1;
     var symbolCode = event.charCode;
-    total.innerHTML = messageLength;
-    leftTotal.innerHTML = leftTotalLength;
+    var symbolKey = event.keyCode;
+
+    console.log(event);
+
+    if(symbolCode !== 0 || symbolKey == 8 || symbolKey == 46 ){
+        total.innerHTML = messageLength;
+        leftTotal.innerHTML = leftTotalLength;
+    }
 
     if(leftTotalLength <= 0 && symbolCode !== 0){
         event.preventDefault();
