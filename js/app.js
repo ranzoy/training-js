94b51797bff2a4a446ea60ad9fab295fce341388
Ihor Shchepilov <i.shchepilov@sysiq.com>
2015-02-23 16:01:46 +0200

    fix keyboard events

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ var maxChar = 10;
 var textChar = document.getElementById('js-message');
 var total = document.getElementById('js-message-total');
 var leftTotal = document.getElementById('js-message-left-symbols');
+var resetForm = document.getElementById('js-reset-btn');
 
 var textLength;
 var leftTotalCount;
@@ -11,23 +12,33 @@ leftTotal.innerHTML = maxChar;
 
 
 var keydownEvent = function(event){
+    var symbolCode = event.keyCode;
 
     textLength = textChar.value.length;
     leftTotalCount = maxChar - textLength;
 
-    if(leftTotalCount <= 0){
+    if(leftTotalCount <=0 && symbolCode !==8 && symbolCode !==35 && symbolCode !==36 && symbolCode !==36 && symbolCode !==37 && symbolCode !==38 && symbolCode !==39 && symbolCode !==40 && symbolCode !==16 && symbolCode !==17 && symbolCode !==46){
         event.preventDefault();
     }
 }
 
 var keyupEvent = function(event) {
 
+    textLength = textChar.value.length;
+    leftTotalCount = maxChar - textLength;
 
-    total.innerHTML = textLength;
-    leftTotal.innerHTML = leftTotalCount;
+    if(leftTotalCount =>0){
+        total.innerHTML = textLength;
+        leftTotal.innerHTML = leftTotalCount;
+    }
 
 }
 
+var resetCounter = function(){
+    total.innerHTML = 0;
+    leftTotal.innerHTML = maxChar;
+}
 
 textChar.addEventListener("keydown", keydownEvent);
-//textChar.addEventListener("keyup", keyupEvent);
+textChar.addEventListener("keyup", keyupEvent);
+resetForm.addEventListener("click", resetCounter);
