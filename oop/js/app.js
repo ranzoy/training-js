3e892c63e318e015c91ecdd2628d28fc683a3c07
Ihor Shchepilov <i.shchepilov@sysiq.com>
2015-02-26 14:02:39 +0200

    add obj

--- oop/js/app.js
+++ oop/js/app.js
@@ -2,7 +2,6 @@
  * TextCounter
  */
 var TextCounter = {
-    self: this,
     maxChar: 10,
     textArea: null,
     textAreaTotal: null,
@@ -13,57 +12,33 @@ var TextCounter = {
         this.textAreaLeft = document.getElementById('js-message-left-symbols');
         this.resetForm = document.getElementById('js-reset-btn');
         this.textLength = this.textArea.value.length;
-
         this.events();
     },
-
     events: function() {
-        this.textArea.addEventListener("keydown", this.keydownEvent);
-        this.textArea.addEventListener("keyup", this.keyupEvent);
-        this.resetForm.addEventListener("click", this.resetCounter);
+        this.textArea.addEventListener("keydown", this.keydownEvent.bind(this));
+        this.textArea.addEventListener("keyup", this.keyupEvent.bind(this));
+        this.resetForm.addEventListener("click", this.resetCounter.bind(this));
     },
     keydownEvent: function(event){
-        console.log(self.maxChar);
         var symbolCode = event.keyCode;
-        //console.log(this.maxChar);
-        //this.textLength = this.textArea.value.length;
-        this.textAreaLeft = this.maxChar - this.textLength;
-
-        if(this.textAreaLeft <=0 && symbolCode !==8 && symbolCode !==35 && symbolCode !==36 && symbolCode !==36 && symbolCode !==37 && symbolCode !==38 && symbolCode !==39 && symbolCode !==40 && symbolCode !==16 && symbolCode !==17 && symbolCode !==46){
+        this.textLength = this.textArea.value.length;
+        this.textAreaLeftCount = this.maxChar - this.textLength;
+        if(this.textAreaLeftCount <=0 && symbolCode !==8 && symbolCode !==35 && symbolCode !==36 && symbolCode !==36 && symbolCode !==37 && symbolCode !==38 && symbolCode !==39 && symbolCode !==40 && symbolCode !==16 && symbolCode !==17 && symbolCode !==46){
             event.preventDefault();
         }
     },
     keyupEvent: function(){
-
+        this.textLength = this.textArea.value.length;
+        this.textAreaLeftCount = this.maxChar - this.textLength;
+        if(this.textAreaLeftCount >= 0){
+            this.textAreaTotal.innerHTML = this.textLength;
+            this.textAreaLeft.innerHTML = this.textAreaLeftCount;
+        }
     },
     resetCounter: function(){
-
+        this.textAreaTotal.innerHTML = 0;
+        this.textAreaLeft.innerHTML = this.maxChar;
     }
 };
 
 TextCounter.init();
-/*var textLength;
-var leftTotalCount;
-leftTotal.innerHTML = maxChar;
-
-var keyupEvent = function(event) {
-
-    textLength = textChar.value.length;
-    leftTotalCount = maxChar - textLength;
-
-    if(leftTotalCount >= 0){
-        total.innerHTML = textLength;
-        leftTotal.innerHTML = leftTotalCount;
-    }
-
-}
-
-var resetCounter = function(){
-    total.innerHTML = 0;
-    leftTotal.innerHTML = maxChar;
-}
-
-textChar.addEventListener("keydown", keydownEvent);
-textChar.addEventListener("keyup", keyupEvent);
-resetForm.addEventListener("click", resetCounter);
-*/
